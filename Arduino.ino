43c27a54081a3283adf9fd0c46ff20ef2bf2a4f5
PZSpasov19 <63718750+PZSpasov19@users.noreply.github.com>
2020-05-22 16:33:38 +0300

    Update Arduino.ino

--- Arduino.ino
+++ Arduino.ino
@@ -1,18 +1,11 @@
 #include <ArduBots.h>
-#define NUM_SENSORS 6
-#define QTR_THRESHOLD  1500
-#define RIGHT 1
-#define LEFT -1
-ArduBotsMotors motors;
-ReflectanceArray sensors(QTR_NO_EMITTER_PIN);
-unsigned int sensor_values[NUM_SENSORS];
+
 void setup() {
   
 }
 void loop() {
-      motors.setSpeeds(0, 0);
-      sensors.read(sensor_values);
-  if (sensor_values[0] < QTR_THRESHOLD)
-  {
-  }
+  motors.setSpeeds(350,350);
+  delay(1000);
+  motors.setSpeed(-350,-350);
+  delay(1000);
 }
